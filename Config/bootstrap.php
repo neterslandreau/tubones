816e086b7ca62c2e2cede1151e8dd18aac87cd7f
Niles <neterslandreau@gmail.com>
2011-09-04 11:54:33 -0400

    change to loading only working plugins, not all.

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -63,4 +63,6 @@ Cache::config('default', array('engine' => 'File'));
  * CakePlugin::load('DebugKit'); //Loads a single plugin named DebugKit
  *
  */
-	CakePlugin::loadAll();
+	CakePlugin::load('DebugKit');
+	CakePlugin::load('Migrations');
+	CakePlugin::load('Users');
